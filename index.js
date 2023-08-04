c37bb0d6efc1ffc7beef77c8b16367ef189aa875
BigBlueHat <byoung2@wiley.com>
2023-08-04 16:06:16 -0400

    Set User-Agent header

--- index.js
+++ index.js
@@ -32,7 +32,11 @@ argv
       try {
         const url = new URL(argv.url);
         axios
-          .get(argv.url)
+          .get(argv.url, {
+            headers: {
+              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0'
+            }
+          })
           .then((res) => {
             // TODO: confirm 200, etc
             extractDatablocks(res.data);
